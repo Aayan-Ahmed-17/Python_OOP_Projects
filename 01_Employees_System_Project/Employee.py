147fef95d8e845c1c83c10845263b639554ae1cc
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-02-11 00:15:34 +0500

    basic employee class created sucesfuly | file name correction

---
+++ 01_Employees_System_Project/Employee.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        
+    def __str__(self):
+        return f"Employee {self.name} is {self.age} yrs old having {self.salary} salary"
+    
+    
+"""Test case"""
+# emp1 = Employee("Aayan Ahmed", 19, 200000)
+# print(emp1)
