653cbd8f0b6bd6acf66d2e29a7f7d7f8fa71c9ef
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-02-12 15:36:44 +0500

    wrote docstring for all | update return statement for all returned list

--- 01_Employees_System_Project/manager.py
+++ 01_Employees_System_Project/manager.py
@@ -7,65 +7,69 @@ This file manages CRUD operations of Employee()
 
 class EmployeeManager:
     def __init__(self):
-        self.employees = [
-            # {"name": "aayan ahmed", "age": 19, "salary": 40000},
-            # {"name": "aayan", "age": 21, "salary": 300000},
-            # {"name": "ahmed", "age": 25, "salary": 1800000},
-        ]
+        self.employees = []
 
     def add_employee(self, name, age, salary):
         """
         PURPOSE: Creates Employee() instance
-        RETURN: Created Employee() instance
+        RETURN: [emp]
         """
         emp = Employee(name, age, salary)
         self.employees.append(emp)
-        return emp
+        return [emp]
 
     # Read Employees (Get All by condition or without)
     def get_employees(self, filter_by=None):
+        """
+        PURPOSE: Get all employees with or without "filter_by"
+        RETURN: [], self.employees
+        """
         if not self.employees:
             return []
-
-        # Handle filter_by salary or name input validation in run function
-        """take a look on to it in future"""
-        # if filter_by:
-        #     result = [
-        #         emp
-        #         for emp in self.employees
-        #         if emp.name == filter_by or emp.salary == filter_by
-        #     ]
 
         return self.employees
 
     def get_one_employee(self, name):
+        """
+        PURPOSE: Get all employees with or without "filter_by"
+        RETURN: [], [emp], []
+        """
+        if not self.employees:
+            return []
+
         for emp in self.employees:
             if emp["name"] == name:
-                return emp
+                return [emp]
 
         return []
 
     # update employee salary by name filter
     def update_salary(self, name, new_salary):
-        # is_empty??
+        """
+        PURPOSE: Update employee salary through name filter
+        RETURN: [], [emp], [] #for not found
+        """
+        if not self.employees:
+            return []
 
         for emp in self.employees:
             if emp.name == name:
                 emp.salary = int(new_salary)
-                return emp
+                return [emp]
 
         return f"Employee {name} not found"
 
     def delete_employee(self, name):
+        """
+        PURPOSE: delete employee through name filter
+        RETURN: [], [emp], [] #for not found
+        """
+        if not self.employees:
+            return []
+
         for emp in self.employees:
             if emp.name == name:
                 self.employees.remove(emp)
-                return f"Employee {name} deleted"
+                return [emp]
 
-        return f"Employee {name} not found"
-
-
-"""Test case"""
-# emp1 = EmployeesManager()
-# print(emp1.add_employee())
-# # print(emp1.employees[0])
+        return []
